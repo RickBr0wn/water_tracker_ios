8e4b1f0d6e4bc9259e094dc984bf8c927c49d42d
Rick Brown <ricky.brown.00@gmail.com>
2021-08-18 14:51:57 +0100

    Initial Commit

---
+++ water tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  water tracker
+//
+//  Created by Rick Brown on 18/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ water tracker/water_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  water_trackerApp.swift
+//  water tracker
+//
+//  Created by Rick Brown on 18/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct water_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
